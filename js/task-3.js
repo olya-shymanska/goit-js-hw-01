ef858a70f6ad1983e798358057a704c5e49cb56b
olya shymanska <olyashymanska3@gmail.com>
2024-11-04 23:59:49 -0600

    task 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function getElementWidth(content, padding, border) {
+    content = parseInt(content);
+    padding = parseInt(padding);
+    border = parseFloat(border);
 
+    const totalWidth = content + (padding * 2) + (border * 2);
+
+    return totalWidth;
 }
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
